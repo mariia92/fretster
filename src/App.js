b0e836c5daa03881c772524e9fe343a7d4b31190
Mariia Tiurina <mariia.tiurina@check24.de>
2022-10-08 15:56:34 +0200

    fix homepage

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import "./App.css";
 
 const App = () => {
   const navigationButtons = [
-    { text: "home", href: "/home" },
+    { text: "home", href: "/" },
     { text: "unterricht", href: "/unterricht" },
     { text: "workshops", href: "/workshops" },
     { text: "lehrer", href: "/lehrer" },
     { text: "events", href: "/events" },
     { text: "kontakt", href: "/kontakt" },
   ];
-  const getCurrentPage = () => navigationButtons.findIndex((b) => b.href === window.location.pathname);
+  const getCurrentPage = () =>
+    navigationButtons.findIndex((b) => b.href === window.location.pathname) ||
+    0;
   const [currentPage, setCurrentPage] = React.useState(getCurrentPage());
 
   const Pages = {
     home: HomePage,
     unterricht: UnterrichtPage,
   };
-  const PageToRender = Pages[navigationButtons[currentPage].text];
+  const PageToRender = Pages[navigationButtons[currentPage]?.text] || HomePage;
 
   return (
     <PageToRender
